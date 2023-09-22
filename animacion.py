2bc3a4733aaf875557984d3b45826029bdb8c326
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2023-09-22 14:18:07 +0200

    Ahora se puede ajustar la velocidad de las animaciones

--- animacion.py
+++ animacion.py
@@ -3,12 +3,21 @@ class Animacion:
         self.imagenes = imagenes
         self.numero_frames = len(imagenes)
         self.frame_actual = 0
+        self.tiempo = 0
+        self.duracion_frame = 10
         
     def obtener_frame_actual(self):
         return self.imagenes[self.frame_actual] 
         
     def siguiente_frame(self):
-        self.frame_actual = (self.frame_actual + 1) % self.numero_frames
+        self.tiempo += 1
+        
+        if self.tiempo >= self.duracion_frame:
+            self.tiempo = 0
+            self.frame_actual += 1
+            
+            if self.frame_actual >= self.numero_frames:
+                self.frame_actual = 0
         
     def reiniciar(self):
         self.frame_actual = 0
