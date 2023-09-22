d1fb87e1a47fe91f883e9c23b36bc1a727663455
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2023-09-22 14:18:48 +0200

    Haciendo pruebas de imagenes y animaciones

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sys import exit
 from ajustes import *
 from mundo import *
+import herramientas_imagen as hi
+from animacion import Animacion
 import pygame
 
 #Iniciar pygame
@@ -12,14 +14,23 @@ reloj = pygame.time.Clock()
 pantalla = pygame.display.set_mode(RESOLUCION)
 mundo = Mundo()
 
+animacion = Animacion(hi.redimensionar_imagenes(hi.cargar_imagenes("res/Actor/Animals/Cat/SpriteSheet.png"), 5))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
             
+            
+     #Limpiar la pantalla
+    pantalla.fill((0, 0, 0))       
+        
     mundo.actualizar()
     mundo.dibujar(pantalla)
+    
+    pantalla.blit(animacion.obtener_frame_actual(), (0, 0))
+    animacion.siguiente_frame()
 
     pygame.display.update()
     reloj.tick(60)
