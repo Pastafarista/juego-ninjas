2e55b570c275149c6282974f74d802defef48f5e
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2023-09-22 14:09:37 +0200

    limpiado el código

--- entidad.py
+++ entidad.py
@@ -2,7 +2,7 @@ import numpy as np
 import pygame
 
 class entidad:
-    def __init__(self, pos_x, pos_y, tam_x, tam_y, velocidad):
+    def __init__(self, pos_x, pos_y, offset_x, offset_y, tam_x, tam_y, velocidad):
         self.posicion = np.array([pos_x, pos_y])
         self.caja_colision = pygame.Rect(offset_x, offset_y, tam_x, tam_y)
         self.velocidad = velocidad
